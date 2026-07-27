c337ea2f994de0ada6ae58d46e3db0e4a7c403db
mirelaglaab-oss <mirela.glaab@escola.pr.gov.br>
2026-07-27 14:52:19 -0300

    egtW3G4

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ const avanca = documet.querySelectorA11('.btn-proximo');
 avanca.forEach(button => {
      button.addEventListener('click', function(){
         const atual = document.querySelector('.ativo')
-        const proximoPasso = 'passo-' + this.getAttribute('')
+        const proximoPasso = 'passo-' + this.getAttribute('data-')
      })
 })
