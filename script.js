522ac372a5d1d65fc954db10b17d23159c1a23b8
mirelaglaab-oss <mirela.glaab@escola.pr.gov.br>
2026-07-27 14:51:53 -0300

    avhavkbkv

---
+++ script.js
@@ -0,0 +1,8 @@
+const avanca = documet.querySelectorA11('.btn-proximo');
+
+avanca.forEach(button => {
+     button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo')
+        const proximoPasso = 'passo-' + this.getAttribute('')
+     })
+})
